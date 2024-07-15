fed57d0aeae9080981ca8b5354764997672afd81
AruzhanIssabekova <150043321+AruzhanIssabekova@users.noreply.github.com>
2024-07-15 11:39:11 +0500

    Add files via upload

---
+++ samplesite1/bboard/views.py
@@ -0,0 +1,34 @@
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Task
+from .forms import TaskForm
+
+class TaskListView(ListView):
+    model = Task
+    template_name = 'task_list.html'
+    context_object_name = 'tasks'
+    success_url = reverse_lazy('task_list')
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'task_detail.html'
+    context_object_name = 'task'
+    success_url = reverse_lazy('task_list')
+
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'task_form.html'
+    success_url = reverse_lazy('task_list')
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'task_form.html'
+    success_url = reverse_lazy('task_list')
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = 'task_confirm_delete.html'
+    success_url = reverse_lazy('task_list')
+
